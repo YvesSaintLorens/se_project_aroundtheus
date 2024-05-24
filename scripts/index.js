0cec0e8c04eedf9113c0a07d3cf7593746f9ece4
Lorens <lorensprof@gmail.com>
2024-05-23 18:20:20 -0700

    added modal and functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-console.log ('Hello')
 
 const initialCards = [
     {
@@ -26,3 +25,34 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
 },
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile_description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__container");
+
+function closePopup() {
+    profileEditModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+    profileEditModal.classList.add("modal_opened");
+});
+
+profileEditCloseButton.addEventListener("click", () => {
+    closePopup();
+});
+
+console.log(profileEditForm);
+profileEditForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+});
